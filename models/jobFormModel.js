0402ee1c6ebb5b0563f80825744701f359b02206
Madonna Adel <madonnadel0@gmail.com>
2024-05-15 20:02:25 +0300

    Madonna

--- models/jobFormModel.js
+++ models/jobFormModel.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const jobFormSchema = new mongoose.Schema({
     jobFormName: { type: String },
-    jobFormDetails:{type:String},
+    jobFormDetails: { type: String },
+    test:String,
+    
 })
 
 module.exports = mongoose.model("jobFormSchema", jobFormSchema);
