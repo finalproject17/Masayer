02a5d369a285b3d5dc7e35176069acb87073e01a
Your Name <safynazabdelraheem@gmail.com>
2024-05-21 16:09:53 +0300

    CompanySchema

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const cors = require('cors');
 const dotenv = require('dotenv').config(); 
 const mongoose = require('mongoose');
 const JobRoute = require('./routes/JobRoute'); 
+const CompanyRoute = require('./routes/CompanyRoutes');
+const auth = require('./middlewares/auth');
 
 // Middleware
 app.use(cors());
 app.use(express.json()); 
+// app.use(middleWares.auth);
 
 // Routes
 app.use('/jobs', JobRoute); 
+
+app.use('/companies', CompanyRoute);
+
+
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO)
