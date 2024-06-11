a353c4982a9ed00d2b1e7caf23246bab6a2234df
shroukgamall17 <shrouqgamal17@gmail.com>
2024-06-11 16:45:11 +0300

    Creating Ahmed branch

--- routes/jobFormRoute.js
+++ routes/jobFormRoute.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const { addJobForm, updateJobForm, deletJobForm, getJobForm } = require("../controllers/jobFormController");
 
 router.get("/", getJobForm);
+
 router.post("/", addJobForm);
 router.put("/:id", updateJobForm);
 router.delete("/:id", deletJobForm);
+
 
 module.exports = router;
